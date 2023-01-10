e7a89539257712db191f3b21f5c5504064235a0a
Mannlex Murillo <mannlexx21@gmail.com>
2023-01-10 11:31:42 -0700

    componentes y listado

--- Angular/01-bases/src/app/app.component.ts
+++ Angular/01-bases/src/app/app.component.ts
@@ -1,17 +1,12 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  titulo: string = 'Contador app';
-  numero: number = 0;
-  base: number = 55;
 
-  acumular(valor: number) {
-    this.numero += valor;
-  }
 }
 
