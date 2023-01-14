2e0fe7813b2592a1e6e6488e016c5070d71bc30a
Mannlex Murillo <mannlexx21@gmail.com>
2023-01-14 11:14:07 -0700

    template, ngif, tap, switchmap, activatedRoute

--- Angular/03-paisesApp/src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ Angular/03-paisesApp/src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,13 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { PaisService } from '../../services/pais.service';
-import { switchMap } from 'rxjs/operators';
+import { switchMap, tap } from 'rxjs/operators';
+import { Country } from '../../interfaces/pais.interface';
 
 @Component({
   selector: 'app-ver-pais',
   templateUrl: './ver-pais.component.html',
 })
 export class VerPaisComponent implements OnInit {
+  pais!: Country;
+
   constructor(
     private activatedRoute: ActivatedRoute,
     private paisService: PaisService
@@ -25,9 +28,12 @@ export class VerPaisComponent implements OnInit {
     //   },
     // });
     this.activatedRoute.params
-      .pipe(switchMap(({ id }) => this.paisService.getPaisPorAlpha(id)))
-      .subscribe((resp) => {
-        console.log(resp);
+      .pipe(
+        switchMap(({ id }) => this.paisService.getPaisPorAlpha(id)),
+        tap((resp) => console.log(resp)) // lo que el tap hace es ejecutar algo cuando recibe la respuesta el subscribe
+      )
+      .subscribe({
+        next: (resp) => (this.pais = resp),
       });
   }
 }
