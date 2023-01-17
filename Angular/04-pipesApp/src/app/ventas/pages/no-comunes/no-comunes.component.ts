0c9ce7110f4d3df76ba2f481e47eb7954a24ae47
Mannlex Murillo <mannlexx21@gmail.com>
2023-01-17 00:38:18 -0700

    i18nPlural

--- Angular/04-pipesApp/src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ Angular/04-pipesApp/src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -8,8 +8,17 @@ export class NoComunesComponent {
   nombre: string = 'Manuel';
   genero: string = 'masculino';
 
+  //i18nSelect
   invitacionMapa = {
     masculino: 'invitarlo',
     femenino: 'invitarla',
+  };
+
+  //i18nPlural
+  clientes = ['Maria', 'Jose', 'Raul'];
+  clientesMapa = {
+    '=0': 'no tenemos ningun cliente',
+    '=1': 'tenemos 1 cliente',
+    other: 'tenemos # clientes',
   };
 }
