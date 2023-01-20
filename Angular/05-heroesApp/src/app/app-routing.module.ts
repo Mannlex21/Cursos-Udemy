22a616859ac8d0eeba0fed5614059f64f075b0fb
Mannlex Murillo <mannlexx21@gmail.com>
2023-01-20 11:23:28 -0700

    app routing module

---
+++ Angular/05-heroesApp/src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { ErrorPageComponent } from './shared/error-page/error-page.component';
+
+const routes: Routes = [
+  { path: '404', component: ErrorPageComponent },
+  { path: '**', redirectTo: '404' },
+];
+
+@NgModule({
+  declarations: [],
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
