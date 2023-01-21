9473822e7e8f556674510cc4ef5e655b7b68e2eb
Mannlex Murillo <mannlexx21@gmail.com>
2023-01-20 18:06:26 -0700

    child routes

--- Angular/05-heroesApp/src/app/heroes/heroes-routing.module.ts
+++ Angular/05-heroesApp/src/app/heroes/heroes-routing.module.ts
@@ -5,10 +5,12 @@ import { AgregarComponent } from './pages/agregar/agregar.component';
 import { HeroeComponent } from './pages/heroe/heroe.component';
 import { ListadoComponent } from './pages/listado/listado.component';
 import { BuscarComponent } from './pages/buscar/buscar.component';
+import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
   {
     path: '',
+    component: HomeComponent,
     children: [
       {
         path: 'agregar',
