23e5a0ff9d4cbe1337e2a0414e6afa29e9ecfc53
Mannlex Murillo <mannlexx21@gmail.com>
2023-01-27 10:38:46 -0700

    useLayoutEffect

--- React/05-hook-app/src/main.jsx
+++ React/05-hook-app/src/main.jsx
@@ -6,6 +6,7 @@ import { FormWithCustomHook } from "./02-useEffect/FormWithCustomHook";
 import { SimpleForm } from "./02-useEffect/SimpleForm";
 import { MultipleCustomHooks } from "./03-examples/MultipleCustomHooks";
 import { FocusScreen } from "./04-useRef/FocusScreen";
+import { Layout } from "./05-useLayoutEffect/Layout";
 import { HooksApp } from "./HooksApp";
 import "./index.css";
 
@@ -17,6 +18,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
 		{/* <SimpleForm></SimpleForm> */}
 		{/* <FormWithCustomHook></FormWithCustomHook> */}
 		{/* <MultipleCustomHooks></MultipleCustomHooks> */}
-		<FocusScreen></FocusScreen>
+		{/* <FocusScreen></FocusScreen> */}
+		<Layout></Layout>
 	</React.StrictMode>
 );
